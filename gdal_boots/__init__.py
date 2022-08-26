ae4eca7b4561bd6ce634bccfd82f7146f32d73b3
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2022-08-26 14:44:21 +0300

    chore: code style

--- gdal_boots/__init__.py
+++ gdal_boots/__init__.py
@@ -1,4 +1,18 @@
-__version__ = '0.1.43'
+__version__ = "0.1.43"
 
 from .gdal import GeoInfo, RasterDataset, Resampling, VectorDataset
-from .options import *
+from .options import ECW, GPKG, PNG, ESRIShape, GeoJSON, GTiff, JP2OpenJPEG
+
+__all__ = [
+    "GeoInfo",
+    "RasterDataset",
+    "Resampling",
+    "VectorDataset",
+    "PNG",
+    "GTiff",
+    "JP2OpenJPEG",
+    "ECW",
+    "ESRIShape",
+    "GeoJSON",
+    "GPKG",
+]
--- gdal_boots/options.py
+++ gdal_boots/options.py
@@ -4,18 +4,19 @@ from enum import Enum
 from osgeo import gdal
 
 __all__ = (
-    'PNG',
-    'GTiff',
-    'JP2OpenJPEG',
-    'ECW',
-    'ESRIShape',
-    'GeoJSON',
-    'GPKG',
+    "PNG",
+    "GTiff",
+    "JP2OpenJPEG",
+    "ECW",
+    "ESRIShape",
+    "GeoJSON",
+    "GPKG",
 )
+
 
 def _encode(values):
     return [
-        ('='.join([name, str(value.value if isinstance(value, Enum) else value)])).upper()
+        ("=".join([name, str(value.value if isinstance(value, Enum) else value)])).upper()
         for name, value in values.items()
         if value is not None
     ]
@@ -32,7 +33,7 @@ class DriverOptions:
     @property
     def driver_extensions(self):
         driver = self.driver
-        return driver.GetMetadataItem(gdal.DMD_EXTENSIONS).split(' ')
+        return driver.GetMetadataItem(gdal.DMD_EXTENSIONS).split(" ")
 
     @property
     def driver(self):
@@ -41,9 +42,9 @@ class DriverOptions:
 
 @dataclass
 class PNG(DriverOptions):
-    '''
-        Portable Network Graphics
-    '''
+    """
+    Portable Network Graphics
+    """
 
     zlevel: int = 6
     nbits: int = None
@@ -51,25 +52,25 @@ class PNG(DriverOptions):
 
 @dataclass
 class GTiff(DriverOptions):
-    '''
-        GeoTIFF File Format
-        https://gdal.org/drivers/raster/gtiff.html
-    '''
+    """
+    GeoTIFF File Format
+    https://gdal.org/drivers/raster/gtiff.html
+    """
 
     class Compress(Enum):
-        lzw = 'LZW'
-        jpeg = 'JPEG'
-        packbits = 'PACKBITS'
-        deflate = 'DEFLATE'
-        zstd = 'ZSTD'
-        webp = 'WEBP'
-        lerc = 'LERC'
-        lerc_deflate = 'LERC_DEFLATE'
-        lerc_zstd = 'LERC_ZSTD'
+        lzw = "LZW"
+        jpeg = "JPEG"
+        packbits = "PACKBITS"
+        deflate = "DEFLATE"
+        zstd = "ZSTD"
+        webp = "WEBP"
+        lerc = "LERC"
+        lerc_deflate = "LERC_DEFLATE"
+        lerc_zstd = "LERC_ZSTD"
 
     class Interleave(Enum):
-        band = 'BAND'
-        pixel = 'PIXEL'
+        band = "BAND"
+        pixel = "PIXEL"
 
     blockxsize: int = 256
     blockysize: int = 256
@@ -88,26 +89,26 @@ class GTiff(DriverOptions):
 
 @dataclass
 class JP2OpenJPEG(DriverOptions):
-    '''
-        JPEG2000 driver based on OpenJPEG library
-        https://gdal.org/drivers/raster/jp2openjpeg.html
-    '''
+    """
+    JPEG2000 driver based on OpenJPEG library
+    https://gdal.org/drivers/raster/jp2openjpeg.html
+    """
 
     quality: float = 25
     resolutions: int = None
     blockxsize: int = 1024
     blockysize: int = 1024
     nbits: int = None
-    tileparts: str = 'disabled'
+    tileparts: str = "disabled"
     write_metadata: bool = True
 
 
 @dataclass
 class ECW(DriverOptions):
-    '''
-        Enhanced Compressed Wavelets
-        https://gdal.org/drivers/raster/ecw.html
-    '''
+    """
+    Enhanced Compressed Wavelets
+    https://gdal.org/drivers/raster/ecw.html
+    """
 
     # Set the target size reduction as a percentage of the original.
     # If not provided defaults to 90% for greyscale images, and 95% for RGB images.
@@ -118,23 +119,23 @@ class ECW(DriverOptions):
 
 @dataclass
 class ESRIShape(DriverOptions):
-    '''
-        https://gdal.org/tutorials/vector_api_tut.html
-    '''
+    """
+    https://gdal.org/tutorials/vector_api_tut.html
+    """
 
 
 @dataclass
 class GeoJSON(DriverOptions):
-    '''
-        https://gdal.org/drivers/vector/geojson.html
-    '''
+    """
+    https://gdal.org/drivers/vector/geojson.html
+    """
 
-    write_bbox = 'NO'
+    write_bbox = "NO"
 
 
 @dataclass
 class GPKG(DriverOptions):
-    '''
-        GeoPackage vector
-        https://gdal.org/drivers/vector/gpkg.html
-    '''
+    """
+    GeoPackage vector
+    https://gdal.org/drivers/vector/gpkg.html
+    """
