82a20fb8fa818d30d419add409fea84cf2141720
Alexander Verbitsky <habibutsu@gmail.com>
2023-01-31 16:16:54 +0300

    update version to 0.1.46

--- gdal_boots/__init__.py
+++ gdal_boots/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.45"
+__version__ = "0.1.46"
 
 from .gdal import GeoInfo, RasterDataset, Resampling, VectorDataset
 from .options import ECW, GPKG, PNG, ESRIShape, GeoJSON, GTiff, JP2OpenJPEG
